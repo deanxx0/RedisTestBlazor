94482a11d7899d3646a154aaef7167b25892dd5c
deanxx <deanxx999@gmail.com>
2021-11-26 16:21:48 +0900

    test redis caching with blazor

---
+++ RedisTest/Utils/DistributedCacheUtil.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RedisTest.Utils
+{
+    // IDistributedCache의 Set, Get 메서드를 래핑한 클래스.
+    public static class DistributedCacheUtil
+    {
+        public static async Task SetRecordAsync<T>(this IDistributedCache cache,
+            string recordId,
+            T data,
+            TimeSpan? absoluteExpireTime = null,
+            TimeSpan? unusedExpireTime = null)
+        {
+            var options = new DistributedCacheEntryOptions();
+            options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60);
+            options.SlidingExpiration = unusedExpireTime;
+
+            var jsonData = JsonSerializer.Serialize(data);
+            await cache.SetStringAsync(recordId, jsonData, options); // 실제 redis에 캐시를 쓰는 부분
+        }
+
+        public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
+        {
+            var jsonData = await cache.GetStringAsync(recordId); // redis에서 캐시를 읽어오는 부분
+            if (jsonData is null)
+            {
+                return default(T);
+            }
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+    }
+}
